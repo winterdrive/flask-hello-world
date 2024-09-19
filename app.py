384d8b0179e1183b6c10c46fb4247bdd0c01c273
winterdrive <90021888+winterdrive@users.noreply.github.com>
2024-09-19 16:04:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+# New API to serve the fb_ui.html page
+@app.route('/fb_ui')
+def fb_ui():
+    return render_template('fb_ui.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
